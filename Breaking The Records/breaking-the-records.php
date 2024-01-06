1da6a368eccd6f15e42e45d866cc25333417c86d
Fatma Alaa <fatalaa1@gmail.com>
2024-01-06 23:32:33 +0200

    add solution for "breaking the records problem"

---
+++ Breaking The Records/breaking-the-records.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords($scores) {
+    // Write your code here
+    $min = 0;
+    $max = 0;
+    $n = count( $scores );
+    $breaking_points = array(0,0);
+    for( $i = 0; $i < $n; $i++ ) {
+        if( $i === 0 ) {
+            $min = $max = $scores[$i];
+        }
+        if( $scores[$i] > $min && $scores[$i] > $max ) {
+            $max = $scores[$i];
+            $breaking_points[0]++;
+        }
+        if ( $scores[$i] < $min ) {
+            $min = $scores[$i];
+            $breaking_points[1]++;
+        }
+    }
+    return $breaking_points;
+}
+
+
+// expected output => 2 4
+print_r( breakingRecords(array(10, 5, 20, 20, 4, 5, 2, 25, 1)) );
+
+// expected output => 4 0
+print_r( breakingRecords(array(3, 4, 21, 36, 10, 28, 35, 5, 24, 42)) );
+
+// expected output => 0 3
+print_r( breakingRecords(array(100000, 9, 3, 10, 2, 20)) );
